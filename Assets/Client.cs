e7a50c5a47d99f217fbe9d70c16132501781e8b7
AronT <aron.thompson@racunarstvo.hr>
2020-10-28 21:15:13 +0100

    Partial code refactor

--- Assets/Client.cs
+++ Assets/Client.cs
@@ -11,140 +11,35 @@ using EventArgs = System.EventArgs;
 
 public class Client : MonoBehaviour
 {
+    private Serializator ser = new Serializator();
     public WebSocket client;
+    private List<string> messages = new List<string>();
+    private List<MessageEventArgs> serverMessages = new List<MessageEventArgs>();
+    private Players players = new Players();
+    private List<GameObject> playerObjects = new List<GameObject>();
+    private bool moveReady = true;
+    private bool worldLoaded = false;
+    private bool writingMessage = false;
+    private World world;
 
+    [Header("UI Elements")]
     public GameObject[] inputs;
     public GameObject[] messagingObj;
     public InputField inputName;
     public InputField messagingField;
     public Text textMessage;
 
-    List<string> messages = new List<string>();
-    Players players = new Players();
-
-    private bool worldLoaded = false;
-    private World world;
-    Serializator s = new Serializator();
-
+    [Header("World Objects")]
     public GameObject hole;
     public GameObject player;
     public GameObject ground;
     public GameObject wall;
-    private List<GameObject> playerObjects = new List<GameObject>();
-
-    List<MessageEventArgs> serverMessages = new List<MessageEventArgs>();
-
-    private bool writingMessage = false;
-
-    private void Update()
-    {
-        lock (serverMessages)
-        {
-            foreach (var msg in serverMessages)
-            {
-                // if is data treat it as world
-                if (msg.IsBinary)
-                {
-                    if (!worldLoaded)
-                    {
-                        world = s.DeserializeWorld(msg.RawData);
-                        worldLoaded = true;
-                        BuildWorld();
-                    }
-                    else
-                    {
-                        players = s.DeserializePlayers(msg.RawData);
-
-                        // Position players
-                        PositionPlayers();
-                    }
-                }
-                // if msg is string
-                if (msg.IsText)
-                {
-                    messages.Add(msg.Data);
-                }
-
-                foreach (var message in messages)
-                {
-                    textMessage.text += message + Environment.NewLine;
-                }
-            }
-            messages.Clear();
-            serverMessages.Clear();
-        }
-
-        
-        if (messagingField.isFocused == true){writingMessage = true;}
-        else{writingMessage = false;}
-    }
-
-    private void PositionPlayers()
-    {
-        if (playerObjects.Count > 0)
-        {
-            foreach (var item in playerObjects)
-            {
-                Destroy(item);
-            }
-        }
-        foreach (var item in players.players)
-        {
-            int posX = item.position % 10;
-            int posZ = item.position / 10;
-            playerObjects.Add(Instantiate(player, new Vector3(posX, 1f, posZ), Quaternion.identity));
-        }
-    }
-
-    public void JoinChatroom(string chatroomPath)
-    {
-        if (inputName.text.Trim() != string.Empty)
-        {
-            client = new WebSocket("ws://localhost:8080/" + chatroomPath);
-            client.OnOpen += Client_OnOpen;
-            client.OnMessage += Client_OnMessage;
-            client.Connect();
-
-            foreach (var button in inputs) { button.SetActive(false); }
-            foreach (var button in messagingObj) { button.SetActive(true); }
-        }
-
-    }
 
     private void Client_OnMessage(object sender, MessageEventArgs e)
     {
         lock (serverMessages)
         {
             serverMessages.Add(e);
-        }
-    }
-
-    private void BuildWorld()
-    {
-        int counter = 0;
-        int zPos = 0;
-        for (int i = 0; i < world.ground.Length; i++)
-        {
-            switch (world.ground[i])
-            {
-                case 0:
-                    Instantiate(wall, new Vector3(counter, 0.5f, zPos), Quaternion.identity);
-                    break;
-                case 1:
-                    Instantiate(ground, new Vector3(counter, 0f, zPos), Quaternion.identity);
-                    break;
-                case 2:
-                    Instantiate(hole, new Vector3(counter, 0f, zPos), Quaternion.identity);
-                    break;
-                default:
-                    break;
-            }
-            counter++;
-            if (counter == 10)
-            {
-                zPos++;
-                counter = 0;
-            }
         }
     }
 
@@ -158,16 +53,6 @@ public class Client : MonoBehaviour
         client.Close();
     }
 
-    public void SendText()
-    {
-        if (messagingField.text.Trim() != string.Empty)
-        {
-            client.Send("#msg:" + messagingField.text);
-            messagingField.text = string.Empty;
-        }
-    }
-
-    bool moveReady = true;
     private void OnGUI()
     {
         Event e = Event.current;
@@ -210,5 +95,114 @@ public class Client : MonoBehaviour
             }
         }
     }
-}
 
+    private void Update()
+    {
+        lock (serverMessages)
+        {
+            foreach (var msg in serverMessages)
+            {
+                // if is data treat it as world
+                if (msg.IsBinary)
+                {
+                    if (!worldLoaded)
+                    {
+                        world = ser.DeserializeWorld(msg.RawData);
+                        worldLoaded = true;
+                        BuildWorld();
+                    }
+                    else
+                    {
+                        players = ser.DeserializePlayers(msg.RawData);
+                        PositionPlayers();
+                    }
+                }
+                // if msg is string
+                if (msg.IsText)
+                {
+                    messages.Add(msg.Data);
+                }
+
+                foreach (var message in messages)
+                {
+                    textMessage.text += message + Environment.NewLine;
+                }
+            }
+            messages.Clear();
+            serverMessages.Clear();
+        }
+
+        if (messagingField.isFocused == true){writingMessage = true;}
+        else{writingMessage = false;}
+    }
+
+    public void JoinChatroom(string chatroomPath)
+    {
+        if (inputName.text.Trim() != string.Empty)
+        {
+            client = new WebSocket("ws://localhost:8080/" + chatroomPath);
+            client.OnOpen += Client_OnOpen;
+            client.OnMessage += Client_OnMessage;
+            client.Connect();
+
+            foreach (var button in inputs) { button.SetActive(false); }
+            foreach (var button in messagingObj) { button.SetActive(true); }
+        }
+
+    }
+
+    public void SendText()
+    {
+        if (messagingField.text.Trim() != string.Empty)
+        {
+            client.Send("#msg:" + messagingField.text);
+            messagingField.text = string.Empty;
+        }
+    }
+
+    private void PositionPlayers()
+    {
+        if (playerObjects.Count > 0)
+        {
+            foreach (var item in playerObjects)
+            {
+                Destroy(item);
+            }
+        }
+        foreach (var item in players.players)
+        {
+            int posX = item.position % 10;
+            int posZ = item.position / 10;
+            playerObjects.Add(Instantiate(player, new Vector3(posX, 1f, posZ), Quaternion.identity));
+        }
+    }
+
+    private void BuildWorld()
+    {
+        int counter = 0;
+        int zPos = 0;
+        for (int i = 0; i < world.ground.Length; i++)
+        {
+            switch (world.ground[i])
+            {
+                case 0:
+                    Instantiate(wall, new Vector3(counter, 0.5f, zPos), Quaternion.identity);
+                    break;
+                case 1:
+                    Instantiate(ground, new Vector3(counter, 0f, zPos), Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(hole, new Vector3(counter, 0f, zPos), Quaternion.identity);
+                    break;
+                default:
+                    break;
+            }
+            counter++;
+            if (counter == 10)
+            {
+                zPos++;
+                counter = 0;
+            }
+        }
+    }
+}
--- MUD_Server/MUD_Server/Program.cs
+++ MUD_Server/MUD_Server/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Text;
 using WebSocketSharp;
 using WebSocketSharp.Server;
 
@@ -15,6 +12,7 @@ namespace MUD_Server
             WebSocketServer server = new WebSocketServer(8080);
 
             server.AddWebSocketService<Chatroom>("/ChatroomA");
+            // For adding second "chatroom"
             // server.AddWebSocketService<Chatroom>("/ChatroomB");
 
             server.Start();
@@ -23,11 +21,12 @@ namespace MUD_Server
         }
     }
 
-
     public class Chatroom : WebSocketBehavior
     {
         Serializator ser = new Serializator();
+        Random random = new Random();
         static World wrld = new World();
+        // unfortunate naming - should have been Data
         static Players players = new Players();
 
         // first string is Uri (meaning ws://localhost:8080/ChatroomA or B or wotever
@@ -37,156 +36,6 @@ namespace MUD_Server
             // Sending world
             Send(ser.serialize(wrld));
             base.OnOpen();
-        }
-
-        protected override void OnMessage(MessageEventArgs e)
-        {
-            // user.ID, user.name
-            Dictionary<string, Player> chatroom = null;
-            if (players.players == null)
-            {
-                players.players = new List<Player>();
-            }
-            // if there is chatroom with users just set it to this
-            // i need this only for multiple chatrooms
-            if (!users.ContainsKey(Context.RequestUri.ToString()))
-            {
-                users[Context.RequestUri.ToString()] = new Dictionary<string, Player>();
-            }
-            // else create new
-            chatroom = users[Context.RequestUri.ToString()];
-
-            // Context is contained within WebSocketBehavior class and .RequestUri checks full Uri, e.g.:ws://localhost:8080/ChatroomA
-            // Console.WriteLine(Context.RequestUri);
-
-            // MINI PROTOCOL
-            // if e.Data starts with "#name:" then set name
-            if (e.Data.StartsWith("#name:"))
-            {
-                Player p = new Player();
-                p.nick = e.Data.Substring(6);
-                p.ID = ID;
-                void PositionPlayer(string iD)
-                {
-                    var random = new Random();
-                    bool searchingFreeSpot = true;
-                    while (searchingFreeSpot)
-                    {
-                        int rndPos = random.Next(10, wrld.ground.Length - 12);
-
-                        // if position is on edges -> skip
-                        if (rndPos % 10 == 0 || (rndPos - 9) % 10 == 0)
-                        {
-                            continue;
-                        }
-
-                        p.position = rndPos;
-                        searchingFreeSpot = false;
-                        foreach (var item in players.players)
-                        {
-                            if (rndPos == item.position)
-                            {
-                                searchingFreeSpot = true;
-                            }
-                        }
-                    }
-                }
-
-                PositionPlayer(ID);
-                chatroom[ID] = p;
-                players.players.Add(p);
-
-                Sessions.Broadcast(ser.serialize(players));
-            }
-            else if (e.Data.StartsWith("#msg:"))
-            {
-                Player tempPlayer = new Player();
-                foreach (var p in players.players)
-                {
-                    if (ID == p.ID)
-                    {
-                        tempPlayer = p;
-                        break;
-                    }
-                }
-                var msg = tempPlayer.nick + ": " + e.Data.Substring(5);
-                Console.WriteLine(msg);
-                // sending message to all clients
-                foreach (var user in chatroom)
-                {
-                    //user.Key is ID
-                   Sessions.SendTo(msg, user.Key);
-                };
-            }
-            else if (e.Data.StartsWith("#plyMove:"))
-            {
-                string move = e.Data.Substring(9);
-
-                Player tempPlayer = new Player();
-                foreach (var p in players.players)
-                {
-                    if (ID == p.ID)
-                    {
-                        tempPlayer = p;
-                        break;
-                    }
-                }
-
-                if (move == "W")
-                {
-                    if (wrld.ground[tempPlayer.position + 10] == 2)
-                    {
-                        return;
-                    }
-                    // he moved up
-                    if (tempPlayer.position + 10 < wrld.ground.Length - 10)
-                    {
-                        tempPlayer.position += 10;
-                        Sessions.Broadcast(ser.serialize(players));
-                    }
-
-                }
-                else if (move == "S")
-                {
-                    if (wrld.ground[tempPlayer.position - 10] == 2)
-                    {
-                        return;
-                    }
-                    // he moved down
-                    if (tempPlayer.position - 10 > 10)
-                    {
-                        tempPlayer.position -= 10;
-                        Sessions.Broadcast(ser.serialize(players));
-                    }
-                }
-                else if (move == "A")
-                {
-                    if (wrld.ground[tempPlayer.position - 1] == 2)
-                    {
-                        return;
-                    }
-                    // he moved left
-                    if ((tempPlayer.position - 1) % 10 != 0)
-                    {
-                        tempPlayer.position--;
-                        Sessions.Broadcast(ser.serialize(players));
-                    }
-                }
-                else if (move == "D")
-                {
-                    if (wrld.ground[tempPlayer.position + 1] == 2)
-                    {
-                        return;
-                    }
-                    // he moved right
-                    if ((tempPlayer.position + 1 - 9) % 10 != 0)
-                    {
-                        tempPlayer.position++;
-                        Sessions.Broadcast(ser.serialize(players));
-                    }
-                }
-            }
-            base.OnMessage(e);
         }
 
         protected override void OnClose(CloseEventArgs e)
@@ -214,8 +63,157 @@ namespace MUD_Server
                     break;
                 }
             }
-
             base.OnClose(e);
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            // user.ID, user.name
+            Dictionary<string, Player> chatroom = null;
+            if (players.players == null)
+            {
+                players.players = new List<Player>();
+            }
+            // if there is chatroom with users just set it to this
+            // i need this only for multiple chatrooms
+            if (!users.ContainsKey(Context.RequestUri.ToString()))
+            {
+                users[Context.RequestUri.ToString()] = new Dictionary<string, Player>();
+            }
+            // else create new
+            chatroom = users[Context.RequestUri.ToString()];
+
+            // Context is contained within WebSocketBehavior class and .RequestUri checks full Uri, e.g.:ws://localhost:8080/ChatroomA
+            // Console.WriteLine(Context.RequestUri);
+
+            // MINI PROTOCOL
+            // if e.Data starts with "#name:" then set name for player - handle joining
+            if (e.Data.StartsWith("#name:"))
+            {
+                Player p = new Player();
+                p.nick = e.Data.Substring(6);
+                p.ID = ID;
+
+                void PositionPlayer()
+                {
+                    bool seekingFreeSpot = true;
+                    while (seekingFreeSpot)
+                    {
+                        int rndPos = random.Next(10, wrld.ground.Length - 12);
+
+                        // if position is on edges -> skip, also avoid holes
+                        if (rndPos % 10 == 0 || (rndPos - 9) % 10 == 0 || wrld.ground[rndPos] == 2)
+                        {
+                            continue;
+                        }
+
+                        // avoid player spots
+                        foreach (var item in players.players)
+                        {
+                            if (rndPos == item.position){continue;}
+                        }
+                        
+                        p.position = rndPos;
+                        seekingFreeSpot = false;
+                    }
+                }
+
+                PositionPlayer();
+                chatroom[ID] = p;
+                players.players.Add(p);
+
+                Sessions.Broadcast(ser.serialize(players));
+            }
+            // handle messaging
+            else if (e.Data.StartsWith("#msg:"))
+            {
+                Player tempPlayer = new Player();
+                foreach (var p in players.players)
+                {
+                    if (ID == p.ID)
+                    {
+                        tempPlayer = p;
+                        break;
+                    }
+                }
+                var msg = tempPlayer.nick + ": " + e.Data.Substring(5);
+                // sending message to all clients
+                foreach (var user in chatroom)
+                {
+                    //user.Key is ID
+                   Sessions.SendTo(msg, user.Key);
+                };
+            }
+            // handle movement
+            else if (e.Data.StartsWith("#plyMove:"))
+            {
+                string move = e.Data.Substring(9);
+
+                Player tempPlayer = new Player();
+                foreach (var p in players.players)
+                {
+                    if (ID == p.ID)
+                    {
+                        tempPlayer = p;
+                        break;
+                    }
+                }
+
+                // he moved up
+                if (move == "W")
+                {
+                    if (wrld.ground[tempPlayer.position + 10] == 2)
+                    {
+                        return;
+                    }
+                    if (tempPlayer.position + 10 < wrld.ground.Length - 10)
+                    {
+                        tempPlayer.position += 10;
+                        Sessions.Broadcast(ser.serialize(players));
+                    }
+
+                }
+                // he moved down
+                else if (move == "S")
+                {
+                    if (wrld.ground[tempPlayer.position - 10] == 2)
+                    {
+                        return;
+                    }
+                    if (tempPlayer.position - 10 > 10)
+                    {
+                        tempPlayer.position -= 10;
+                        Sessions.Broadcast(ser.serialize(players));
+                    }
+                }
+                // he moved left
+                else if (move == "A")
+                {
+                    if (wrld.ground[tempPlayer.position - 1] == 2)
+                    {
+                        return;
+                    }
+                    if ((tempPlayer.position - 1) % 10 != 0)
+                    {
+                        tempPlayer.position--;
+                        Sessions.Broadcast(ser.serialize(players));
+                    }
+                }
+                // he moved right
+                else if (move == "D")
+                {
+                    if (wrld.ground[tempPlayer.position + 1] == 2)
+                    {
+                        return;
+                    }
+                    if ((tempPlayer.position + 1 - 9) % 10 != 0)
+                    {
+                        tempPlayer.position++;
+                        Sessions.Broadcast(ser.serialize(players));
+                    }
+                }
+            }
+            base.OnMessage(e);
         }
     }
 }
